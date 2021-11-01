518c74a1da683877a91ab0b2580713cddbafa16a
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2021-11-01 11:27:42 +0100

    comm

--- target/classes/static/js/shoppingCartUnregistered.js
+++ target/classes/static/js/shoppingCartUnregistered.js
@@ -1,1 +1,172 @@
-$(document).ready(function(){$(".firstDaljeBtn").on("click",function(a){if(Cookies.get("korisnik")!==undefined){var b=JSON.parse(Cookies.get("korisnik"));console.log(b);$("#predzadnjiKorakIme").val(b.ime);$("#predzadnjiKorakPrezime").val(b.prezime);$("#predzadnjiKorakTelefon").val(b.telefon);$("#predzadnjiKorakEmail").val(b.email);$("#predzadnjiKorakAdresa").val(b.adresa);$("#predzadnjiKorakPB").val(b.postanskibroj);$("#predzadnjiKorakGrad").val(b.grad);$("#nacinPlacanja").val(b.nacinplacanja)}else{console.log("nije pronadjen korisnik")}});$("#korpaStavkeNEREG").append(napraviKorpaStavke());updateTotals();$(".skloni").on("click",function(a){id=$(this).attr("pid");boja=$(this).attr("boja");$('.stavkaJS[pid="'+id+'"][boja='+boja+"]").slideUp("slow",function(){$('.stavkaJS[pid="'+id+'"][boja="'+boja+'"]').remove();skloniIzCookieKorpe(id,boja);updateTotals()})});$(".smanjiJS").on("click",function(b){field=$('.kolicinaJS[pid="'+$(this).attr("pid")+'"]');var a=parseInt($(field).val());if(!isNaN(a)&&a>1){$(field).val(a-1);updateTotals();updateCookie($(this).attr("pid"),a-1)}});$(".povecajJS").on("click",function(b){field=$('.kolicinaJS[pid="'+$(this).attr("pid")+'"]');var a=parseInt($(field).val());$(field).val(a+1);updateTotals();updateCookie($(this).attr("pid"),a+1)});$("#predzadnjiKorak").on("click",function(a){$("#korpaStavkePregledNEREG").append(napraviKorpaStavkePregled())});$(".JSprevious").on("click",function(a){$(".stavkaPregledJS").remove()});$(".JSzadnjiKorak").on("click",function(f){var d=Cookies.get("korpa");console.log(d);if(d!=="[]"){var g=new Object();g.ime=$("#predzadnjiKorakIme").val();g.prezime=$("#predzadnjiKorakPrezime").val();g.telefon=$("#predzadnjiKorakTelefon").val();g.email=$("#predzadnjiKorakEmail").val();g.adresa=$("#predzadnjiKorakAdresa").val();g.postanskibroj=$("#predzadnjiKorakPB").val();g.grad=$("#predzadnjiKorakGrad").val();g.nacinplacanja=$("#nacinPlacanja").val();g.napomena=$("#predzadnjiKorakNapomena").val();var c=JSON.stringify(g);Cookies.set("korisnik",c);var a=JSON.parse(Cookies.get("korpa"));var b={korisnik:g,korpa:a};url="/korpa/zavrsiPorudzbinu/neregistrovan";$.ajax({type:"GET",contentType:"application/json; charset=utf-8",url:url,data:{myData:JSON.stringify(b)},cache:false,timeout:600000,success:function(e){if(e=="Illegal address"){alert("Uneti email nije ispravan")}else{if(e=='For input string: ""'){alert("Uneti broj telefona ili poštanski broj nisu ispravni")}else{$("#alertHvalaPorudzbina").css("display","block");Cookies.set("korpa","[]")}}},error:function(h){console.log("ovo je error "+h)}})}else{alert("Porudžbina je već poslata")}})});function napraviKorpaStavkePregled(){var e="";if(Cookies.get("korpa")!==undefined){var a=JSON.parse(Cookies.get("korpa"));var d="";for(var c=0;c<a.length;c++){if(a[c].boja!=0){d=' <img class="color-circle" src="/boja/'+a[c].boja+'" > '}else{d=" <span>Boja: osnovna</span>"}var b='<div class="row border-top border-bottom stavkaPregledJS korpaStavkeSmallerScreen" pid="'+a[c].idProizvoda+'">            <div class="row main align-items-center mt-2 mb-2 w-100">           <div class="col-4 slikaStavkaSmallerScreen"><img class="img-fluid slika-korpaStavkaPregled  slika-korpaStavkaSmallerScreen"  src="/photo/'+a[c].idProizvoda+"/"+a[c].photoId+'" alt="'+a[c].alt_text+'"  title="'+a[c].title+'"></div>          <div class="col-2 nazivOpisSmallerScreen">          <div class="row naslov"><span>'+a[c].nazivProizvoda+'</span></div>         </div><div class="col-3 product-color-basket product-color-basketSmallerScreen">'+d+'</div>         <div class="cenaprSmallerScreenStavkaPregled">        <div class="col cenaprSmallerScreen cenaprSmallerScreenStavkaPregled" style="font-weight: 500">Kolicina:  <span style="font-weight: 600" class="kolicinaPregledJS" >'+a[c].kolicina+' </span> </div>        <div class="col cenaprSmallerScreen cenaprSmallerScreenStavkaPregled" style="font-weight: 500; padding-bottom: 1rem;">Cena: <br><span style="font-weight: 600" >'+a[c].cena+"</span> RSD </div>        </div>        </div>        </div>";e+=b}}return e};
+
+$(document).ready(function () {
+
+
+    $(".firstDaljeBtn").on("click", function (e) {
+        if (Cookies.get('korisnik') !== undefined) {
+            var korisnik = JSON.parse(Cookies.get('korisnik'));          
+               console.log(korisnik);      
+            $("#predzadnjiKorakIme").val(korisnik.ime);
+            $("#predzadnjiKorakPrezime").val(korisnik.prezime);
+            $("#predzadnjiKorakTelefon").val(korisnik.telefon);
+            $("#predzadnjiKorakEmail").val(korisnik.email);
+            $("#predzadnjiKorakAdresa").val(korisnik.adresa);
+            $("#predzadnjiKorakPB").val(korisnik.postanskibroj);
+            $("#predzadnjiKorakGrad").val(korisnik.grad);
+            $("#nacinPlacanja").val(korisnik.nacinplacanja);
+        }
+        else{
+            console.log("nije pronadjen korisnik");
+            
+        }
+    });
+
+
+    $("#korpaStavkeNEREG").append(napraviKorpaStavke());
+    updateTotals();
+
+    $(".skloni").on("click", function (e) {
+        id = $(this).attr("pid");
+        boja = $(this).attr("boja");
+
+        $('.stavkaJS[pid="' + id + '"][boja=' + boja + ']').slideUp("slow", function () {
+            $('.stavkaJS[pid="' + id + '"][boja="' + boja + '"]').remove();
+            skloniIzCookieKorpe(id, boja);
+            updateTotals();
+        });
+    });
+
+    $(".smanjiJS").on("click", function (e) {
+
+
+        field = $('.kolicinaJS[pid="' + $(this).attr("pid") + '"]');
+        var currentVal = parseInt($(field).val());
+        if (!isNaN(currentVal) && currentVal > 1) {
+            $(field).val(currentVal - 1);
+            updateTotals();
+            updateCookie($(this).attr("pid"), currentVal - 1);
+        }
+
+    });
+
+
+
+    $(".povecajJS").on("click", function (e) {
+        field = $('.kolicinaJS[pid="' + $(this).attr("pid") + '"]');
+        var currentVal = parseInt($(field).val());
+        $(field).val(currentVal + 1);
+        updateTotals();
+        updateCookie($(this).attr("pid"), currentVal + 1);
+    });
+
+    $("#predzadnjiKorak").on("click", function (e) {
+
+        $("#korpaStavkePregledNEREG").append(napraviKorpaStavkePregled());
+    });
+    $(".JSprevious").on("click", function (e) {
+
+        $('.stavkaPregledJS').remove();
+    });
+
+
+
+    $(".JSzadnjiKorak").on("click", function (e) {
+        var celaKorpaJSON = Cookies.get('korpa');
+        console.log(celaKorpaJSON);
+        if (celaKorpaJSON !== "[]") {
+            var korisnik = new Object();
+            korisnik.ime = $("#predzadnjiKorakIme").val();
+            korisnik.prezime = $("#predzadnjiKorakPrezime").val();
+            korisnik.telefon = $("#predzadnjiKorakTelefon").val();
+            korisnik.email = $("#predzadnjiKorakEmail").val();
+            korisnik.adresa = $("#predzadnjiKorakAdresa").val();
+            korisnik.postanskibroj = $("#predzadnjiKorakPB").val();
+            korisnik.grad = $("#predzadnjiKorakGrad").val();
+            korisnik.nacinplacanja = $("#nacinPlacanja").val();
+            korisnik.napomena = $("#predzadnjiKorakNapomena").val();
+            var korisnikJSON = JSON.stringify(korisnik);
+            Cookies.set('korisnik', korisnikJSON);
+
+            var celaKorpa = JSON.parse(Cookies.get('korpa'));
+
+            var myData = {
+                "korisnik": korisnik,
+                "korpa": celaKorpa
+            };
+
+            url = "/korpa/zavrsiPorudzbinu/neregistrovan";
+            $.ajax({
+                type: "GET",
+                contentType: 'application/json; charset=utf-8',
+                url: url,
+                data: {"myData": JSON.stringify(myData)},
+
+                cache: false,
+                timeout: 600000,
+                success: function (response) {
+                    // alert(response);
+                    if (response == "Illegal address") {
+                        alert("Uneti email nije ispravan");
+                    } else {
+                        if (response == "For input string: \"\"") {
+                            alert("Uneti broj telefona ili poštanski broj nisu ispravni");
+
+                        } else {
+                            $("#alertHvalaPorudzbina").css("display", "block");
+                            Cookies.set('korpa', "[]");
+                        }
+                    }
+                },
+                error: function (e) {
+                    //  alert(e);
+                    console.log("ovo je error " + e);
+                }
+            });
+        } else {
+            alert("Porudžbina je već poslata");
+        }
+
+    });
+
+});
+
+
+
+
+function napraviKorpaStavkePregled() {
+    var stavke = "";
+    if (Cookies.get('korpa') !== undefined) {
+        var celaKorpa = JSON.parse(Cookies.get('korpa'));
+        var bojatekst = "";
+        for (var i = 0; i < celaKorpa.length; i++) {
+
+            if (celaKorpa[i].boja != 0) {
+                bojatekst = " <img class=\"color-circle\" src=\"/boja/" + celaKorpa[i].boja + "\" > ";
+            } else {
+                bojatekst = " <span>Boja: osnovna</span>";
+
+            }
+
+            var stavka = "<div class=\"row border-top border-bottom stavkaPregledJS korpaStavkeSmallerScreen\" pid=\"" + celaKorpa[i].idProizvoda + "\">"
+                    + "            <div class=\"row main align-items-center mt-2 mb-2 w-100\">"
+                    + "           <div class=\"col-4 slikaStavkaSmallerScreen\"><img class=\"img-fluid slika-korpaStavkaPregled  slika-korpaStavkaSmallerScreen\"  src=\"/photo/" + celaKorpa[i].idProizvoda + "/" + celaKorpa[i].photoId + "\" alt=\"" + celaKorpa[i].alt_text + "\"  title=\"" + celaKorpa[i].title + "\"></div>"
+                    + "          <div class=\"col-2 nazivOpisSmallerScreen\">"
+                    + "          <div class=\"row naslov\"><span>" + celaKorpa[i].nazivProizvoda + "</span></div>"
+                    + "         </div>"
+                    + "<div class=\"col-3 product-color-basket product-color-basketSmallerScreen\">"
+                    + bojatekst
+                    + "</div>"
+
+
+                    + "         <div class=\"cenaprSmallerScreenStavkaPregled\">"
+                    + "        <div class=\"col cenaprSmallerScreen cenaprSmallerScreenStavkaPregled\" style=\"font-weight: 500\">Kolicina:  <span style=\"font-weight: 600\" class=\"kolicinaPregledJS\" >" + celaKorpa[i].kolicina + " </span> </div>"
+                    + "        <div class=\"col cenaprSmallerScreen cenaprSmallerScreenStavkaPregled\" style=\"font-weight: 500; padding-bottom: 1rem;\">Cena: <br><span style=\"font-weight: 600\" >" + celaKorpa[i].cena + "</span> RSD </div>"
+                    + "        </div>"
+                    + "        </div>"
+                    + "        </div>";
+            stavke += stavka;
+        }
+    }
+    return stavke;
+}
+
