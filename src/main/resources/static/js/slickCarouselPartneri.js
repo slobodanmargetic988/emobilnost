a000ffbf2a5d1356c7f90c9fff948fc56a526137
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2021-11-16 14:50:27 +0100

    commit

--- src/main/resources/static/js/slickCarouselPartneri.js
+++ src/main/resources/static/js/slickCarouselPartneri.js
@@ -1,7 +1,34 @@
+//$(document).ready(function () {
+//    $('.customer-logos').slick({
+//       
+//        slidesToShow: 5,
+//        slidesToScroll: 2,
+//        autoplay: true,
+//        autoplaySpeed: 3000,
+//        arrows: false,
+//        dots: false,
+//        pauseOnHover: false,
+//        responsive: [{
+//                breakpoint: 767,
+//                settings: {
+//                    slidesToShow: 4
+//                }
+//            }, {
+//                breakpoint: 520,
+//                settings: {
+//                    slidesToShow: 3
+//                }
+//            }]
+//    });
+//      $('.slick-track').attr('aria-label', 'Our customers logos');
+//             
+//              
+//});
+
 $(document).ready(function () {
     $('.customer-logos').slick({
        
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 2,
         autoplay: true,
         autoplaySpeed: 3000,
@@ -11,7 +38,7 @@ $(document).ready(function () {
         responsive: [{
                 breakpoint: 767,
                 settings: {
-                    slidesToShow: 4
+                    slidesToShow: 3
                 }
             }, {
                 breakpoint: 520,
